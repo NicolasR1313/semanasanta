6bf3f283e8a5ffb27dc08e83b0d1a761c5d8ec91
Nicolas Rios <126140343+NicolasR1313@users.noreply.github.com>
2025-04-07 14:12:05 -0500

    Update entrypoint.sh

--- semanasanta-main/semanasanta/entrypoint.sh
+++ semanasanta-main/semanasanta/entrypoint.sh
@@ -5,7 +5,7 @@ mkdir -p /mnt/volume_town/uploads
 
 # Dar permisos para que PHP pueda escribir
 chown -R www-data:www-data /mnt/volume_town/uploads
-chmod -R 755 /mnt/volume_town/uploads
+chmod -R 777 /mnt/volume_town/uploads
 
 # Iniciar Apache
 exec apache2-foreground
