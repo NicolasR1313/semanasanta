a1f45164513866d25978fe396c79129665dfaf4a
Nicolas Rios <126140343+NicolasR1313@users.noreply.github.com>
2025-04-29 09:04:55 -0500

    Update inicio.php

--- semanasanta-main/semanasanta/inicio.php
+++ semanasanta-main/semanasanta/inicio.php
@@ -216,8 +216,8 @@ $result = mysqli_query($conn, $query);
 </head>
 <body>
 <div class="corner-images">
-    <img src="https://res.cloudinary.com/dsktdsxik/image/upload/v1745614157/1pagina-removebg-preview_dd2fhn.png" class="img-pagina1">
-    <img src="https://res.cloudinary.com/dsktdsxik/image/upload/v1745614157/2pagina-removebg-preview_jvb8ch.png" class="img-pagina2">
+    <img src="https://res.cloudinary.com/dsktdsxik/image/upload/v1745935311/logo_cenigraf-02_pa61ux.png" class="img-pagina1">
+    <img src="https://res.cloudinary.com/dsktdsxik/image/upload/v1745935416/logo_evento_diaArte_Mesa_de_trabajo_1_anparc.png" class="img-pagina2">
 </div>
 
 
