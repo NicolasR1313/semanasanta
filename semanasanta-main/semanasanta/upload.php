3dc0770940e5b76c19779c3809bb52cff1b33ed3
Nicolas Rios <126140343+NicolasR1313@users.noreply.github.com>
2025-04-29 16:21:52 -0500

    Update upload.php

--- semanasanta-main/semanasanta/upload.php
+++ semanasanta-main/semanasanta/upload.php
@@ -136,7 +136,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['archivo'])) {
 .header-flex h1 {
     font-size: 2.2em;
     color: white;
-    margin: 20px 0;
+    margin: 10px 0;
     border-bottom: 4px double #d4af37;
     display: inline-block;
     padding-bottom: 10px;
