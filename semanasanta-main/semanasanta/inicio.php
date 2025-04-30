0c39c302f50d726e80110a019c6a3166b2d4b6f4
Nicolas Rios <126140343+NicolasR1313@users.noreply.github.com>
2025-04-30 12:47:30 -0500

    Update inicio.php

--- semanasanta-main/semanasanta/inicio.php
+++ semanasanta-main/semanasanta/inicio.php
@@ -340,7 +340,7 @@ $result = mysqli_query($conn, $query);
     </div>
 
     <div class="card-angie">
-        <a href="https://www.youtube.com/watch?v=video2" target="_blank">
+        <a href="https://youtu.be/krdpA6b81rM" target="_blank">
             <img src="https://res.cloudinary.com/dsktdsxik/image/upload/v1745962705/032e435c-49d5-4034-972c-2389bbcca3e5_q14wt2.jpg" alt="Video 2">
             <h3>Youtube: Video sobre el barroco</h3>
             <p>Hecho por: Angie Lorena Sierra</p>
